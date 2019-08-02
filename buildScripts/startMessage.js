c25718122a7746da10d641d5157757e21aabd1b9
svsaurabh <svsaurabh3232@gmail.com>
2019-08-02 18:04:12 +0530

    Travis CI in-progress

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,4 @@
 var chalk = require('chalk');
 
-console.log(chalk.green('Starting application in dev node ...'));
+
+console.log(chalk.green('Starting application in dev node ...'));//eslint-disable-line no-console
---
+++ src/index.test.js
@@ -0,0 +1,20 @@
+const expect = require('chai').expect;
+const jsdom = require('jsdom');
+const fs = require('fs');
+describe('Our First Test',() =>{
+   it('should pass', () =>{
+      expect(true).to.equal(true);
+   });
+});
+
+describe('index.html',() => {
+   it('should say hello', (done) => {
+      const index = fs.readFileSync('./src/index.html',"utf-8");
+      jsdom.env(index, function(err, window){
+         const h1 = window.document.getElementsByTagName('h1')[0];
+         expect(h1.innerHTML).to.equal("Hello World ...!");
+         done();
+         window.close();
+      })
+   })
+})
